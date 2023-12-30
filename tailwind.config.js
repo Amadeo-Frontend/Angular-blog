3c617ee92f2302feef4b39431c7b7ce6b9247f6a
Amadeo Bon <amadeosph@outlook.com>
2023-12-29 23:56:34 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      ssm: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        primary: "#111111",
+        secondary: "#f2f2f2",
+        accent: "#F13024",
+        third: "rgba(50, 64, 255, 0.9831582291119573)",
+      },
+      backgroundImage: {
+        explosion: 'url("/361.png")',
+        circles: 'url("/bg-circles.png")',
+        circleStar: 'url("/circle-star.svg")',
+        site: 'url("/site-bg.svg")',
+      },
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+    },
+  },
+  container: {
+    padding: {
+      DEFAULT: "15px",
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
